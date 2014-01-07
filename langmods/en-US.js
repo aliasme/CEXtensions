2201917cec1a5bea932c287a3982abfdab3ab65e
bitstoned <BM-NBzbPgpfSEn8JZsHip9QeBT5xGi21WGQ>
2014-01-06 22:06:24 -0500

    langmods/en-US: Apply overrides on doc ready

--- langmods/en-US.js
+++ langmods/en-US.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name       CEX.io en-US tweaks
 // @namespace  https://github.com/bitstoned/CEXtensions
-// @version    0.1
+// @version    0.2
 // @description  Overwrite strings with jQuery
 // @match      https://cex.io/
 // @match      https://www.cex.io/
 // @license    GPLv2
 // ==/UserScript==
 
-$('form#buy .btn').text('Bid');
-$('form#sell .btn').text('Ask');
-$('.chat-head > .head-text').text('Trollbox');
+$(document).ready(function() {
+	$('form#buy .btn').text('Bid');
+	$('form#sell .btn').text('Ask');
+});
